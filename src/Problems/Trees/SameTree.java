f1be97885c631807d8de1d4d7a8699890c1fd0d1
Andrei-c1 <andreii.s949@gmail.com>
2024-02-17 22:41:43 +0200

    Same tree tree problem

---
+++ src/Problems/Trees/SameTree.java
@@ -0,0 +1,30 @@
+package Problems.Trees;
+
+class SameTree
+{
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return dfs(p,q);
+
+
+    }
+
+    boolean dfs(TreeNode p,TreeNode q){
+        if(p==null && q == null)
+            return true;
+
+        if(p==null || q == null)
+            return false;
+
+        if(p.val != q.val)
+            return false;
+
+        boolean left = dfs(p.left,q.left);
+        boolean right = dfs(p.right,q.right);
+
+        return left && right;
+
+
+
+    }
+
+}
