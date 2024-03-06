e8bd35964e12ec3cf88d5de73d566496690b7e91
Andrei-c1 <andreii.s949@gmail.com>
2024-03-06 16:44:23 +0200

    TwoSum 2  problem

---
+++ src/Problems/TwoPointer/TwoSum2.java
@@ -0,0 +1,23 @@
+package Problems.TwoPointer;
+
+public class TwoSum2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length-1;
+
+        while(start < end){
+            if(numbers[start]+numbers[end] == target){
+                return new int[] {start+1,end+1};
+            }
+            if(numbers[start]+numbers[end] > target){
+                end--;
+            }
+            else{
+                start++;
+            }
+        }
+
+        return new int[]{} ;
+
+    }
+}
